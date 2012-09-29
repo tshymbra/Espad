a064300b26659435a674e9c41404aba31b9b645a
Taras Shymbra <tshymbra@gmail.com>
2012-09-29 12:22:40 +0200

    removed call to genenames.cmd

--- httpush/prepare.cmd
+++ httpush/prepare.cmd
@@ -5,9 +5,9 @@ xcopy eventdefs\*.names edefs_%1
 xcopy eventdefs\*.pl edefs_%1
 xcopy eventdefs\*.cmd edefs_%1
 cd edefs_%1
-call genenames.cmd
+del *.csv
+perl gen.pl
 del *.names
 del *.pl
-del *.cmd
 cd ..
 cd ../../../
