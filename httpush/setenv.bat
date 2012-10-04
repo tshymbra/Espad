21456848d8047a8a281d8e18cdc69c533a83497b
Taras Shymbra <tshymbra@gmail.com>
2012-10-04 22:48:11 +0200

    updated for Esper 4.6

--- httpush/setenv.bat
+++ httpush/setenv.bat
@@ -14,12 +14,19 @@ if not exist "%JAVA_HOME%\bin\java.exe" (
 
 set EE_HOME=..
 
+if "%HTTPUSH_CLASSPATH_SET%" == "" (
+  set HTTPUSH_CLASSPATH_SET=true
+  goto setclasspath
+) else (
+  goto EOF
+)
+
+:setclasspath
 set CLASSPATH=.;%CLASSPATH%
 set CLASSPATH=%CLASSPATH%;target\classes
-
-set CLASSPATH=%CLASSPATH%;target\lib\esper-4.2.0.jar
-set CLASSPATH=%CLASSPATH%;target\lib\esperio-csv-4.2.0.jar
-set CLASSPATH=%CLASSPATH%;target\lib\esperio-http-4.2.0.jar
+set CLASSPATH=%CLASSPATH%;target\lib\esper-4.6.0.jar
+set CLASSPATH=%CLASSPATH%;target\lib\esperio-csv-4.6.0.jar
+set CLASSPATH=%CLASSPATH%;target\lib\esperio-http-4.6.0.jar
 set CLASSPATH=%CLASSPATH%;target\lib\httpcore-4.0.1.jar
 set CLASSPATH=%CLASSPATH%;target\lib\httpclient-4.0.1.jar
 set CLASSPATH=%CLASSPATH%;target\lib\httpcore-nio-4.0.1.jar
@@ -28,3 +35,5 @@ set CLASSPATH=%CLASSPATH%;target\lib\commons-logging-1.1.1.jar
 set CLASSPATH=%CLASSPATH%;target\lib\log4j-1.2.16.jar
 set CLASSPATH=%CLASSPATH%;target\lib\antlr-runtime-3.2.jar
 set CLASSPATH=%CLASSPATH%;target\lib\ei-1.0.0.jar
+
+:EOF
